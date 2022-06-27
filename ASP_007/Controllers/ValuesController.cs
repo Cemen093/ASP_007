9a619725d99c50797fbe4ad4ade5227ff1c77ea7
Cemen093 <71636411+Cemen093@users.noreply.github.com>
2022-06-27 08:58:55 +0300

    Добавьте файлы проекта.

---
+++ ASP_007/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_007.Controllers
+{
+    /*ASP_007*/
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello My Api";
+        }
+    }
+}
